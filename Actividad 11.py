52c54b9470ad55fb9cea7c046ac7bcd22c71bdde
Josehdz23 <jose.jahr.2016@gmail.com>
2025-08-04 19:34:20 -0600

    Primer ejercicio hecho

--- Actividad 11.py
+++ Actividad 11.py
@@ -8,4 +8,16 @@ def quick_sort(lista):
     mayores = [x for x in lista[1:] if x > pivote]
 
     return quick_sort(menores) + iguales + quick_sort(mayores)
-
+nombres = []
+def main():
+    while True:
+        try:
+            n = int(input("\nCuántos nombres desea agregar: "))
+            for i in range(n):
+                nombre = input(f"Nombre {i+1}: ")
+                nombres.append(nombre)
+            resultado = quick_sort(nombres)
+            print(resultado)
+        except Exception as ex:
+            print(f" Error reintente: {ex}")
+main()
